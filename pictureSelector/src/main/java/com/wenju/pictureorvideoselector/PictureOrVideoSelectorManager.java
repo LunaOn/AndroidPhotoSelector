9b99e782370be371e48a842d42312e7c13434ced
wenju <1148205031@qq.com>
2021-04-16 18:20:54 +0800

    问题修复

--- pictureSelector/src/main/java/com/wenju/pictureorvideoselector/PictureOrVideoSelectorManager.java
+++ pictureSelector/src/main/java/com/wenju/pictureorvideoselector/PictureOrVideoSelectorManager.java
@@ -2,13 +2,13 @@ package com.wenju.pictureorvideoselector;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 
 import com.luck.picture.lib.PictureSelector;
 import com.luck.picture.lib.app.PictureAppMaster;
 import com.luck.picture.lib.config.PictureConfig;
 import com.luck.picture.lib.config.PictureMimeType;
-import com.luck.picture.lib.entity.LocalMedia;
 import com.luck.picture.lib.language.LanguageConfig;
 import com.luck.picture.lib.style.PictureSelectorUIStyle;
 import com.luck.picture.lib.tools.ScreenUtils;
@@ -22,6 +22,7 @@ import java.util.List;
  * Description: 图片视频选择器
  */
 public class PictureOrVideoSelectorManager {
+    public final static int CHOOSE_REQUEST = 188;
 
     /**
      * @param type 查看类型，全部.PictureMimeType.ofAll()、图片.ofImage()、视频.ofVideo()、音频.ofAudio()
@@ -63,16 +64,16 @@ public class PictureOrVideoSelectorManager {
     }
 
     public static void pictureSelector(Context context){
-        pictureSelector(context,PictureMimeType.ofImage(),com.luck.picture.lib.R.drawable.picture_send_button_default_bg,
-                com.luck.picture.lib.R.drawable.picture_send_button_bg,9,false,false,1,1,false,null);
+        pictureSelector(context,PictureMimeType.ofImage(),9,R.drawable.picture_send_button_default_bg,
+                R.drawable.picture_send_button_bg,false,false,1,1,false,null);
     }
 
     /**
      * @param maxSelectNum 最大图片选择数量
      */
     public static void pictureSelector(Context context,int maxSelectNum){
-        pictureSelector(context,PictureMimeType.ofImage(),com.luck.picture.lib.R.drawable.picture_send_button_default_bg,
-                com.luck.picture.lib.R.drawable.picture_send_button_bg,maxSelectNum,false,false,1,1,false,null);
+        pictureSelector(context,PictureMimeType.ofImage(),maxSelectNum,R.drawable.picture_send_button_default_bg,
+                R.drawable.picture_send_button_bg,false,false,1,1,false,null);
     }
 
     /**
@@ -80,8 +81,8 @@ public class PictureOrVideoSelectorManager {
      * @param drawableTopCompleteNormalBtnBackground 顶部确认按钮选择样式
      */
     public static void pictureSelector(Context context,int drawableTopCompleteDefaultBtnBackground,int drawableTopCompleteNormalBtnBackground){
-        pictureSelector(context,PictureMimeType.ofImage(),drawableTopCompleteDefaultBtnBackground,
-                drawableTopCompleteNormalBtnBackground,9,false,false,1,1,false,null);
+        pictureSelector(context,PictureMimeType.ofImage(),9,drawableTopCompleteDefaultBtnBackground,
+                drawableTopCompleteNormalBtnBackground,false,false,1,1,false,null);
     }
 
     /**
@@ -90,8 +91,8 @@ public class PictureOrVideoSelectorManager {
      * @param drawableTopCompleteNormalBtnBackground 顶部确认按钮选择样式
      */
     public static void pictureSelector(Context context,int maxSelectNum,int drawableTopCompleteDefaultBtnBackground,int drawableTopCompleteNormalBtnBackground){
-        pictureSelector(context,PictureMimeType.ofImage(),drawableTopCompleteDefaultBtnBackground,
-                drawableTopCompleteNormalBtnBackground,maxSelectNum,false,false,1,1,false,null);
+        pictureSelector(context,PictureMimeType.ofImage(),maxSelectNum,drawableTopCompleteDefaultBtnBackground,
+                drawableTopCompleteNormalBtnBackground,false,false,1,1,false,null);
     }
 
     /**
@@ -100,18 +101,18 @@ public class PictureOrVideoSelectorManager {
      * @param aspectatioY  // 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
      */
     public static void pictureIsCropSelector(Context context,boolean isCrop,int aspectRatioX,int aspectatioY){
-        pictureSelector(context,PictureMimeType.ofImage(),com.luck.picture.lib.R.drawable.picture_send_button_default_bg,
-                com.luck.picture.lib.R.drawable.picture_send_button_bg,9,false,isCrop,aspectRatioX,aspectatioY,false,null);
+        pictureSelector(context,PictureMimeType.ofImage(),9,R.drawable.picture_send_button_default_bg,
+                R.drawable.picture_send_button_bg,false,isCrop,aspectRatioX,aspectatioY,false,null);
     }
 
     public static void pictureisSingleSelectSelector(Context context){
-        pictureSelector(context,PictureMimeType.ofImage(),com.luck.picture.lib.R.drawable.picture_send_button_default_bg,
-                com.luck.picture.lib.R.drawable.picture_send_button_bg,9,true,false,1,1,false,null);
+        pictureSelector(context,PictureMimeType.ofImage(),9,R.drawable.picture_send_button_default_bg,
+                R.drawable.picture_send_button_bg,true,false,1,1,false,null);
     }
 
     public static void VideoSelectSelector(Context context){
-        pictureSelector(context,PictureMimeType.ofVideo(),com.luck.picture.lib.R.drawable.picture_send_button_default_bg
-                ,com.luck.picture.lib.R.drawable.picture_send_button_bg,9,false,false,1,1,false,null);
+        pictureSelector(context,PictureMimeType.ofVideo(),9,R.drawable.picture_send_button_default_bg
+                ,R.drawable.picture_send_button_bg,false,false,1,1,false,null);
     }
 
 
@@ -126,21 +127,21 @@ public class PictureOrVideoSelectorManager {
         uiStyle.picture_container_backgroundColor = Color.parseColor("#000000");
         uiStyle.picture_switchSelectNumberStyle = true;
         uiStyle.picture_navBarColor = Color.parseColor("#393a3e");
-        uiStyle.picture_check_style = com.luck.picture.lib.R.drawable.picture_wechat_num_selector;
-        uiStyle.picture_top_leftBack = com.luck.picture.lib.R.drawable.picture_icon_back;
+        uiStyle.picture_check_style = R.drawable.picture_wechat_num_selector;
+        uiStyle.picture_top_leftBack = R.drawable.picture_icon_back;
         uiStyle.picture_top_titleRightTextColor = new int[]{Color.parseColor("#53575e"), Color.parseColor("#FFFFFF")};
         uiStyle.picture_top_titleRightTextSize = 14;
         uiStyle.picture_top_titleTextSize = 16;
-        uiStyle.picture_top_titleArrowUpDrawable = com.luck.picture.lib.R.drawable.picture_icon_arrow_up;
-        uiStyle.picture_top_titleArrowDownDrawable = com.luck.picture.lib.R.drawable.picture_icon_arrow_down;
+        uiStyle.picture_top_titleArrowUpDrawable = R.drawable.picture_icon_arrow_up;
+        uiStyle.picture_top_titleArrowDownDrawable = R.drawable.picture_icon_arrow_down;
         uiStyle.picture_top_titleTextColor = Color.parseColor("#FFFFFF");
         uiStyle.picture_top_titleBarBackgroundColor = Color.parseColor("#393a3e");
-        uiStyle.picture_top_titleAlbumBackground = com.luck.picture.lib.R.drawable.picture_album_bg;
+        uiStyle.picture_top_titleAlbumBackground = R.drawable.picture_album_bg;
 
         uiStyle.picture_album_textSize = 16;
-        uiStyle.picture_album_backgroundStyle = com.luck.picture.lib.R.drawable.picture_item_select_bg;
+        uiStyle.picture_album_backgroundStyle = R.drawable.picture_item_select_bg;
         uiStyle.picture_album_textColor = Color.parseColor("#4d4d4d");
-        uiStyle.picture_album_checkDotStyle = com.luck.picture.lib.R.drawable.picture_orange_oval;
+        uiStyle.picture_album_checkDotStyle = R.drawable.picture_orange_oval;
 
         uiStyle.picture_bottom_previewTextSize = 16;
         uiStyle.picture_bottom_previewTextColor = new int[]{Color.parseColor("#9b9b9b"), Color.parseColor("#FFFFFF")};
@@ -151,32 +152,32 @@ public class PictureOrVideoSelectorManager {
         uiStyle.picture_adapter_item_camera_backgroundColor = Color.parseColor("#999999");
         uiStyle.picture_adapter_item_camera_textColor = Color.parseColor("#FFFFFF");
         uiStyle.picture_adapter_item_camera_textSize = 14;
-        uiStyle.picture_adapter_item_camera_textTopDrawable = com.luck.picture.lib.R.drawable.picture_icon_camera;
+        uiStyle.picture_adapter_item_camera_textTopDrawable = R.drawable.picture_icon_camera;
 
         uiStyle.picture_adapter_item_textSize = 12;
         uiStyle.picture_adapter_item_textColor = Color.parseColor("#FFFFFF");
-        uiStyle.picture_adapter_item_video_textLeftDrawable = com.luck.picture.lib.R.drawable.picture_icon_video;
-        uiStyle.picture_adapter_item_audio_textLeftDrawable = com.luck.picture.lib.R.drawable.picture_icon_audio;
+        uiStyle.picture_adapter_item_video_textLeftDrawable = R.drawable.picture_icon_video;
+        uiStyle.picture_adapter_item_audio_textLeftDrawable = R.drawable.picture_icon_audio;
 
         uiStyle.picture_bottom_originalPictureTextSize = 14;
-        uiStyle.picture_bottom_originalPictureCheckStyle = com.luck.picture.lib.R.drawable.picture_original_wechat_checkbox;
+        uiStyle.picture_bottom_originalPictureCheckStyle = R.drawable.picture_original_wechat_checkbox;
         uiStyle.picture_bottom_originalPictureTextColor = Color.parseColor("#FFFFFF");
         uiStyle.picture_top_titleRightTextDefaultBackground = drawableTopCompleteDefaultBtnBackground;
         uiStyle.picture_top_titleRightTextNormalBackground = drawableTopCompleteNormalBtnBackground;
         Context appContext = PictureAppMaster.getInstance().getAppContext();
         if (appContext != null) {
             uiStyle.picture_top_titleBarHeight = ScreenUtils.dip2px(appContext, 48);
-            uiStyle.picture_top_titleRightDefaultText = com.luck.picture.lib.R.string.picture_send;
-            uiStyle.picture_top_titleRightNormalText = com.luck.picture.lib.R.string.picture_cancel;
+            uiStyle.picture_top_titleRightDefaultText = R.string.picture_send;
+            uiStyle.picture_top_titleRightNormalText = R.string.picture_cancel;
             uiStyle.picture_bottom_barHeight = ScreenUtils.dip2px(appContext, 45);
-            uiStyle.picture_bottom_previewDefaultText = com.luck.picture.lib.R.string.picture_preview;
-            uiStyle.picture_bottom_previewNormalText = com.luck.picture.lib.R.string.picture_preview_num;
-            uiStyle.picture_bottom_originalPictureText = com.luck.picture.lib.R.string.picture_original_image;
-            uiStyle.picture_bottom_completeDefaultText = com.luck.picture.lib.R.string.picture_please_select;
-            uiStyle.picture_bottom_completeNormalText = com.luck.picture.lib.R.string.picture_completed;
-            uiStyle.picture_adapter_item_camera_text = com.luck.picture.lib.R.string.picture_take_picture;
-            uiStyle.picture_bottom_selectedText = com.luck.picture.lib.R.string.picture_select;
-            uiStyle.picture_bottom_selectedCheckStyle = com.luck.picture.lib.R.drawable.picture_wechat_select_cb;
+            uiStyle.picture_bottom_previewDefaultText = R.string.picture_preview;
+            uiStyle.picture_bottom_previewNormalText = R.string.picture_preview_num;
+            uiStyle.picture_bottom_originalPictureText = R.string.picture_original_image;
+            uiStyle.picture_bottom_completeDefaultText = R.string.picture_please_select;
+            uiStyle.picture_bottom_completeNormalText = R.string.picture_completed;
+            uiStyle.picture_adapter_item_camera_text = R.string.picture_take_picture;
+            uiStyle.picture_bottom_selectedText = R.string.picture_select;
+            uiStyle.picture_bottom_selectedCheckStyle = R.drawable.picture_wechat_select_cb;
             // 如果文本内容设置(%1$d/%2$d)，请开启true
             uiStyle.isCompleteReplaceNum = true;
             uiStyle.picture_top_titleArrowLeftPadding = ScreenUtils.dip2px(appContext, 3);
@@ -184,14 +185,87 @@ public class PictureOrVideoSelectorManager {
             uiStyle.picture_bottom_selectedTextSize = 16;
             uiStyle.picture_bottom_gallery_height = ScreenUtils.dip2px(appContext, 80);
             uiStyle.picture_bottom_gallery_backgroundColor = Color.parseColor("#a0393a3e");
-            uiStyle.picture_bottom_gallery_frameBackground = com.luck.picture.lib.R.drawable.picture_preview_gallery_border_bg;
+            uiStyle.picture_bottom_gallery_frameBackground = R.drawable.picture_preview_gallery_border_bg;
         }
 
         return uiStyle;
     }
 
-    public static List<LocalMedia> getData(List<LocalMedia> list) {
-        return list == null ? new ArrayList<>() : list;
+    public static List<com.luck.picture.lib.entity.LocalMedia> getData(List<LocalMedia> mylist) {
+        if(mylist == null){
+            mylist = new ArrayList<>();
+        }
+        List<com.luck.picture.lib.entity.LocalMedia> list = new ArrayList<>();
+        for (int i = 0; i < mylist.size();i++){
+            com.luck.picture.lib.entity.LocalMedia localMedia = new com.luck.picture.lib.entity.LocalMedia();
+            localMedia.setAndroidQToPath(mylist.get(i).getAndroidQToPath());
+            localMedia.setBucketId(mylist.get(i).getBucketId());
+            localMedia.setChecked(mylist.get(i).isChecked());
+            localMedia.setChooseModel(mylist.get(i).getChooseModel());
+            localMedia.setCompressed(mylist.get(i).isCompressed());
+            localMedia.setCompressPath(mylist.get(i).getCompressPath());
+            localMedia.setCut(mylist.get(i).isCut());
+            localMedia.setCutPath(mylist.get(i).getCutPath());
+            localMedia.setDuration(mylist.get(i).getDuration());
+            localMedia.setFileName(mylist.get(i).getFileName());
+            localMedia.setWidth(mylist.get(i).getWidth());
+            localMedia.setHeight(mylist.get(i).getHeight());
+            localMedia.setId(mylist.get(i).getId());
+            localMedia.setMaxSelectEnabledMask(mylist.get(i).isMaxSelectEnabledMask());
+            localMedia.setMimeType(mylist.get(i).getMimeType());
+            localMedia.setNum(mylist.get(i).getNum());
+            localMedia.setOrientation(mylist.get(i).getOrientation());
+            localMedia.setOriginal(mylist.get(i).isOriginal());
+            localMedia.setOriginalPath(mylist.get(i).getOriginalPath());
+            localMedia.setRealPath(mylist.get(i).getRealPath());
+            localMedia.setPosition(mylist.get(i).getPosition());
+            localMedia.setSize(mylist.get(i).getSize());
+            localMedia.setParentFolderName(mylist.get(i).getParentFolderName());
+            localMedia.setPath(mylist.get(i).getPath());
+            list.add(localMedia);
+        }
+        return list;
+    }
+
+    /**
+     * @param data
+     * @return Selector Multiple LocalMedia
+     */
+    public static List<LocalMedia> obtainMultipleResult(Intent data) {
+        List<LocalMedia> list = new ArrayList<>();
+        if (data != null) {
+            List<com.luck.picture.lib.entity.LocalMedia> result = data.getParcelableArrayListExtra(PictureConfig.EXTRA_RESULT_SELECTION);
+            for (int i = 0; i < result.size();i++){
+                LocalMedia localMedia = new LocalMedia();
+                localMedia.setAndroidQToPath(result.get(i).getAndroidQToPath());
+                localMedia.setBucketId(result.get(i).getBucketId());
+                localMedia.setChecked(result.get(i).isChecked());
+                localMedia.setChooseModel(result.get(i).getChooseModel());
+                localMedia.setCompressed(result.get(i).isCompressed());
+                localMedia.setCompressPath(result.get(i).getCompressPath());
+                localMedia.setCut(result.get(i).isCut());
+                localMedia.setCutPath(result.get(i).getCutPath());
+                localMedia.setDuration(result.get(i).getDuration());
+                localMedia.setFileName(result.get(i).getFileName());
+                localMedia.setWidth(result.get(i).getWidth());
+                localMedia.setHeight(result.get(i).getHeight());
+                localMedia.setId(result.get(i).getId());
+                localMedia.setMaxSelectEnabledMask(result.get(i).isMaxSelectEnabledMask());
+                localMedia.setMimeType(result.get(i).getMimeType());
+                localMedia.setNum(result.get(i).getNum());
+                localMedia.setOrientation(result.get(i).getOrientation());
+                localMedia.setOriginal(result.get(i).isOriginal());
+                localMedia.setOriginalPath(result.get(i).getOriginalPath());
+                localMedia.setRealPath(result.get(i).getRealPath());
+                localMedia.setPosition(result.get(i).getPosition());
+                localMedia.setSize(result.get(i).getSize());
+                localMedia.setParentFolderName(result.get(i).getParentFolderName());
+                localMedia.setPath(result.get(i).getPath());
+                list.add(localMedia);
+            }
+
+        }
+        return list;
     }
 
 }
